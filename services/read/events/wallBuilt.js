948cbcf589eaf4138828ec0aee068f6b47e0ad56
Jonatan Pedersen <jp@jonatanpedersen.com>
2016-01-06 23:10:46 +0100

    fix await default value bug

--- services/read/events/wallBuilt.js
+++ services/read/events/wallBuilt.js
@@ -1,10 +1,12 @@
 export function createHandleWallBuilt(read, write) {
   return async function handle(event) {
-    let wallList = await read('wallList') || [];
+    let wallList = await read('wallList');
+    wallList = wallList || [];
     wallList.push(event.data.wallId);
     await write('wallList', wallList);
 
-    let stats = await read('stats') || {};
+    let stats = await read('stats');
+    stats = stats || {};
     if (!stats.wallsBuilt) {
       stats.wallsBuilt = 0;
     }
@@ -12,7 +14,8 @@ export function createHandleWallBuilt(read, write) {
     await write('stats', stats);
 
     let id = `wall-${event.data.wallId}`;
-    let wall = await read(id) || {};
+    let wall = await read(id);
+    wall = wall || {};
     wall.private = false;
     wall.wallId = event.data.wallId;
     wall.lines = [];
