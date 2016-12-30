412ac8649ef307d5065e1ee3931287806ccbb12c
Jonatan Pedersen <jp@jonatanpedersen.com>
2016-12-30 13:57:35 +0100

    moves main function to main.js and adds babel register to index.js

---
+++ services/read/main.js
@@ -0,0 +1,85 @@
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+import mongodb from 'mongodb';
+import amqp from 'amqplib';
+import { Validator } from 'jsonschema';
+
+// lib
+import { createConnectToMongoDB, createHandleRequest, createGetRequestBody, createGetRequestQuery, createDispatch, createCompositeHandle, createValidateUsingJsonSchema } from  '../../lib';
+import { createReadFromMongoDB, createWriteToMongoDB, createSubscribeToRabbitMQAndDispatch } from '../../lib/read';
+
+// events
+import events from '../../events';
+
+// handle events
+import { createHandleWallBuilt } from  './events/wallBuilt';
+import { createHandleWallCleaned } from  './events/wallCleaned';
+import { createHandleWallDrawnOn } from  './events/wallDrawnOn';
+import { createHandleWallMadePrivate } from  './events/wallMadePrivate';
+import { createHandleWallMadePublic } from  './events/wallMadePublic';
+import { createHandleWallWrittenOn } from  './events/wallWrittenOn';
+
+// queries
+import queries from '../../queries';
+
+// handle queries
+import { createHandleStats } from  './queries/stats';
+import { createHandleWall } from  './queries/wall';
+import { createHandleWallList } from  './queries/wallList';
+
+export async function main () {
+  try {
+    let db = await createConnectToMongoDB(mongodb)('mongodb://localhost:27017/read');
+    let read = createReadFromMongoDB(db);
+    let write = createWriteToMongoDB(db);
+
+    let subscribeToRabbitMQAndDispatch = createSubscribeToRabbitMQAndDispatch(amqp, createDispatch({
+      wallBuilt: () => createHandleWallBuilt(read, write),
+      wallCleaned: () => createHandleWallCleaned(read, write),
+      wallDrawnOn: () => createHandleWallDrawnOn(read, write),
+      wallMadePrivate: () => createHandleWallMadePrivate(read, write),
+      wallMadePublic: () => createHandleWallMadePublic(read, write),
+      wallWrittenOn: () => createHandleWallWrittenOn(read, write)
+    }));
+
+    subscribeToRabbitMQAndDispatch();
+
+    function createValidateWithJsonHandleWithReadFromMongoDB(schema, createHandle) {
+      return createCompositeHandle(
+        createValidateUsingJsonSchema(
+          new Validator(),
+          schema
+        ),
+        createHandle(read)
+      );
+    }
+
+    async function compositionRoot(request, response) {
+      try {
+        let handleRequest = createHandleRequest(
+          createGetRequestQuery(),
+          createDispatch({
+            stats: () => createHandleStats(read),
+            wall: () => createValidateWithJsonHandleWithReadFromMongoDB(queries.wall, createHandleWall),
+            wallList: () => createHandleWallList(read)
+          })
+        );
+
+        await handleRequest(request, response);
+      }
+      catch(ex) {
+        console.log(ex);
+      }
+    }
+
+    let httpServer = http.createServer(compositionRoot);
+    let httpsServer = https.createServer({ key: fs.readFileSync('./server.key', 'utf8'), cert: fs.readFileSync('./server.crt', 'utf8') }, compositionRoot);
+
+    httpServer.listen(20080, () => { console.log('read listening on port 20080'); });
+    httpsServer.listen(20443, () => { console.log('read listening on port 20443'); });
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
--- services/ui/index.js
+++ services/ui/index.js
@@ -1,13 +1,3 @@
-import fs from 'fs';
-import http from 'http';
-import https from 'https';
-import express from 'express';
-
-let app = express();
-app.use('/', express.static('./services/ui/public'));
-
-let httpServer = http.createServer(app);
-let httpsServer = https.createServer({ key: fs.readFileSync('./server.key', 'utf8'), cert: fs.readFileSync('./server.crt', 'utf8') }, app);
-
-httpServer.listen(30080, () => { console.log('ui listening on port 30080'); });
-httpsServer.listen(30443, () => { console.log('ui listening on port 30443'); });
+require('babel-core/register');
+const main = require('./main').main;
+main();
